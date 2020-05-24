d489c18d3fd95e38b920aea358d45551be7e9f85
Caroline Choi <38522055+carolinechoi@users.noreply.github.com>
2020-05-24 11:27:51 -0700

    Create EvenorOdd.js

---
+++ JavaScript Code Challenges/EvenorOdd.js
@@ -0,0 +1,10 @@
+// Even or Odd?
+// Write a function that takes a number as an argument and returns "even" for even numbers or "odd" for odd numbers.
+
+myNumb = 5;
+
+if(myNumb % 2 == 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
